e28b32a668a571c996e1aa1cbc79e6505264c120
vestige <vestige_sync@me.com>
2011-11-15 07:46:18 +0900

    Initial Commit

---
+++ TTT/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  TTT
+#
+#  Created by Makoto Yonezawa on 11/11/15.
+#  Copyright 2011年 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+  attr_accessor :window
+  def applicationDidFinishLaunching(a_notification)
+    # Insert code here to initialize your application
+  end
+end
+
