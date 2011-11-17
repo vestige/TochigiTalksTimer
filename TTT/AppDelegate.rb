15e73d68f02dee4d4d7f90d7784923cba1ee6837
vestige <vestige_sync@me.com>
2011-11-18 08:07:49 +0900

    add select box

--- TTT/AppDelegate.rb
+++ TTT/AppDelegate.rb
@@ -1,15 +1,29 @@
-#
-#  AppDelegate.rb
-#  TTT
-#
-#  Created by Makoto Yonezawa on 11/11/15.
-#  Copyright 2011年 __MyCompanyName__. All rights reserved.
-#
-
 class AppDelegate
   attr_accessor :window
+  attr_accessor :remain
+  attr_accessor :select_time
+  
   def applicationDidFinishLaunching(a_notification)
     # Insert code here to initialize your application
+    setup_time_listbox()
+  end
+
+  def setup_time_listbox()
+    select_time.removeAllItems()
+    select_time.insertItemWithTitle("5",
+                                    atIndex:0)
+    select_time.synchronizeTitleAndSelectedItem()
+    select_time.insertItemWithTitle("15",
+                                    atIndex:1)
+    select_time.synchronizeTitleAndSelectedItem()
+    select_time.insertItemWithTitle("30",
+                                    atIndex:2)
+    select_time.synchronizeTitleAndSelectedItem()
+    select_time.selectItemAtIndex(2)
+  end
+  
+  def update_select(sender)
+    p select_time.selectedItem.title
   end
 end
 
